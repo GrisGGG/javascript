45768d2aefec1164c3fddaeceeebe94b50a09a2f
GrisGGG <astropoetscapicua@gmail.com>
2024-02-25 23:54:50 -0600

    Iniciando modulo de DOM

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,20 @@
+/**DOM
+ * Interfaz de plataforma que proporsiona un conjunto estandar de objetos
+ * para representar objetos html y xml
+ */
+
+let elemento;
+
+elemento = document;
+elemento = document.all;
+elemento = document.head;
+elemento = document.body;
+elemento = document.domain;
+elemento = document.forms;
+elemento = document.forms[0];
+elemento = document.forms[0].id;
+elemento = document.forms[0].method;
+(elemento = document.forms[0]), classList;
+elemento = document.forms[0].className;
+elemento = document.images;
+elemento = document.scripts;
---
+++ 13-DOM/js/02-scripts.js
@@ -0,0 +1,11 @@
+const header = document.getElementsByClassName("header");
+console.log(header);
+
+const hero = document.getElementsByClassName("hero");
+console.log(hero);
+
+const contenedor = document.getElementsByClassName("contenedor");
+console.log(contenedor);
+
+const noExiste = document.getElementsByClassName("noExiste");
+console.log(noExiste);
