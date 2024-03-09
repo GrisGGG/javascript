99407250ff77e95c2ba630f0619b5cd98749c7f7
GrisGGG <astropoetscapicua@gmail.com>
2024-03-08 23:44:07 -0600

    Añadiendo y ocultando alertas

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const inputEmail = document.querySelector("#email");
   const inputAsunto = document.querySelector("#asunto");
   const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
   //Asignar eventos
   inputEmail.addEventListener("blur", validar);
@@ -10,6 +11,31 @@ document.addEventListener("DOMContentLoaded", function () {
   inputMensaje.addEventListener("blur", validar);
 
   function validar(e) {
-    console.log(e.target.value);
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio.`,
+        e.target.parentElement
+      );
+      return;
+    }
+    limpiarAlerta(e.target.parentElement);
+  }
+
+  function mostrarAlerta(mensaje, referencia) {
+    //comprueba si ya existe una alerta
+    limpiarAlerta(referencia);
+    //Generar alerta html
+    const error = document.createElement("P");
+    error.textContent = mensaje;
+    error.classList.add("bg-red-600", "text-white", "p-2", "text-center");
+    //Inyectar el error en el formulario
+    referencia.appendChild(error);
+  }
+
+  function limpiarAlerta(referencia) {
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
+    }
   }
 });
