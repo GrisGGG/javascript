fafbbc7ce807ae0c60bdabf0951561d392c4c45a
GrisGGG <astropoetscapicua@gmail.com>
2024-03-18 23:41:07 -0600

    Reseteando el formulario

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -13,11 +13,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnFormulario = document.querySelector(
     "#formulario button[type='submit']"
   );
+  const btnReset = document.querySelector("#formulario button[type='reset']");
 
   //Asignar eventos
   inputEmail.addEventListener("input", validar);
   inputAsunto.addEventListener("input", validar);
   inputMensaje.addEventListener("input", validar);
+
+  btnReset.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    email.email = "";
+    email.asunto = "";
+    email.mensaje = "";
+    formulario.reset();
+    comprobarEmail();
+  });
 
   function validar(e) {
     if (e.target.value.trim() === "") {
